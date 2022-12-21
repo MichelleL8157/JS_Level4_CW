e30b093c0526f49ca6e56e8df53dfb754350ebb8
MichelleL8157 <98603675+MichelleL8157@users.noreply.github.com>
2022-12-21 04:09:28 -0500

    Add files via upload

---
+++ comment.js
@@ -0,0 +1,7 @@
+/*
+Michelle Liu, Pd. 78
+In this classwork, I learned how to use arrays and the methods that come along with it, such as 
+splice() and push(), which were used in some of the sections of the level. The use of arrays
+helps simplify and group up a bunch of variables and values that are related but not the same
+(for organization).
+*/
